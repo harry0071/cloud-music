885dfcf56339d7ee24170488b0c7864f1515b8bc
stage <1311012326@qq.com>
2018-04-26 16:01:55 +0800

    点击li添加class.li-active

--- src/js/song_list.js
+++ src/js/song_list.js
@@ -1,10 +1,12 @@
 {
 	let view = {
 		el: '.song-list',
-		template:`
+		template: `
 		`,
-		render(data){
-			let {songs} = data;
+		render(data) {
+			let {
+				songs
+			} = data;
 			let $liList = songs.map((item, index) => {
 				return $(`<li>${item.songName}</li>`);
 			});
@@ -12,39 +14,62 @@
 			$liList.forEach((item, index) => {
 				$(this.el).append(item)
 			});
+		},
+		activeLi(li){
+			let $li = $(li);
+			$li.addClass('li-active').siblings('.li-active').removeClass('li-active');
 		}
 	};
 
 	let model = {
-		data:{
+		data: {
 			//songs:[{id:1,songName:'1'},{id:2,songName:'2'}]
-			songs:[]
+			songs: []
 		},
-		find(){
+		find() {
 
-			var query = new AV.Query('Song');//对应数据库中的Class名称
-		return query.find().then((datas) => {//将这个promise给return出去
-    		//console.log(datas);获取全部数据
-    		datas.forEach( (item, index) => {
-    			this.data.songs.unshift({id:item.id,...item.attributes})
-    		});
-		});
+			var query = new AV.Query('Song'); //对应数据库中的Class名称
+			return query.find().then((datas) => { //将这个promise给return出去
+				//console.log(datas);获取全部数据
+				datas.forEach((item, index) => {
+					this.data.songs.unshift({
+						id: item.id,
+						...item.attributes
+					})
+				});
+			});
 		},
 	};
 
 	let controller = {
-		init(view,model){
+		init(view, model) {
 			this.view = view;
 			this.model = model;
 			this.view.render(this.model.data);
-			window.eventHub.listen('saveSong',(songData)=>{
+			this.bindEvents();
+			this.getAllSongs();
+			this.bindEventHub();
+
+
+		},
+		bindEvents() {
+			$(this.view.el).on('click', 'li',(ev) =>{
+				this.view.activeLi(ev.currentTarget);
+			});
+		},
+		bindEventHub() {
+			window.eventHub.listen('saveSong', (songData) => {
 				this.model.data.songs.unshift(songData);
 				this.view.render(this.model.data);
 			});
-			this.model.find().then(()=>{this.view.render(this.model.data)});
-		}
-		
+		},
+		getAllSongs(){
+						this.model.find().then(() => {
+				this.view.render(this.model.data)
+			});
+		},
+
 	};
 
-	controller.init(view,model);
+	controller.init(view, model);
 }
